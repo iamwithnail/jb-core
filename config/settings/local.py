96337fab95cc167959112ba49be4f0b00945f458
Chris Campbell <chris@ccampbell.co>
2018-08-22 11:52:48 +0300

    First main commit, skeleton structure and models.

---
+++ config/settings/local.py
@@ -0,0 +1,90 @@
+from .base import *  # noqa
+from .base import env
+import os
+
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env('DJANGO_SECRET_KEY', default='n16SdfL2SNgUGnfIOB5E87LxlUQVbK497YrudI6ErFDsyrtCvk7TkLBSDA7HwGIs')
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = [
+    "localhost",
+    "0.0.0.0",
+    "127.0.0.1",
+]
+
+# CACHES
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#caches
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': ''
+    }
+}
+
+# TEMPLATES
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#templates
+TEMPLATES[0]['OPTIONS']['debug'] = DEBUG  # noqa F405
+
+# EMAIL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-host
+EMAIL_HOST = 'localhost'
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-port
+EMAIL_PORT = 1025
+
+# django-debug-toolbar
+# ------------------------------------------------------------------------------
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
+INSTALLED_APPS += ['debug_toolbar']  # noqa F405
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']  # noqa F405
+# https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
+DEBUG_TOOLBAR_CONFIG = {
+    'DISABLE_PANELS': [
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+    ],
+    'SHOW_TEMPLATE_CONTEXT': True,
+}
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
+INTERNAL_IPS = ['127.0.0.1', '10.0.2.2']
+
+# testing, might not commit to repo
+# django-extensions
+# ------------------------------------------------------------------------------
+# https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
+INSTALLED_APPS += ['django_extensions']  # noqa F405
+# https://django-storages.readthedocs.io/en/latest/#installation
+INSTALLED_APPS += ['storages']  # noqa F405
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+AWS_ACCESS_KEY_ID = env('DJANGO_AWS_ACCESS_KEY_ID')
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+AWS_SECRET_ACCESS_KEY = env('DJANGO_AWS_SECRET_ACCESS_KEY')
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+AWS_STORAGE_BUCKET_NAME = env('DJANGO_AWS_STORAGE_BUCKET_NAME')
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+    os.path.join(ROOT_DIR, 'jobsolutely/static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_QUERYSTRING_AUTH = False
+# DO NOT change these unless you know what you're doing.
+_AWS_EXPIRY = 60 * 60 * 24 * 7
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': f'max-age={_AWS_EXPIRY}, s-maxage={_AWS_EXPIRY}, must-revalidate',
+}
+
+# Your stuff...
+# ------------------------------------------------------------------------------
---
+++ core/models.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+# Create your models here.
+
+class ContactDetails(models.Model):
+    # abstractclass
+    telephone = models.TextField()
+    street_address = models.CharField(max_length=30)
+    town = models.CharField(max_length=30)
+    postcode = models.CharField(max_length=30)
+    email = models.EmailField()
+
+
+class Skill(models.Model):
+    description = models.TextField()
+    skill_code = models.CharField(max_length=30)
+
+
+class ContractorContactDetails(ContactDetails):
+    field = models.TextField()
+    name = models.TextField()
+
+
+class HirerContactDetails(ContactDetails):
+    invoice_contact = models.EmailField()
+
+
+RATE_CHOICES = (
+    (1, 'Hourly'),
+    (2, 'Daily'),
+    (3, 'Weekly'),
+    (4, '4 Weekly'),
+    (5, 'Monthly'),
+    (6, 'Annually')
+)
+
+
+class Role(models.Model):
+    title = models.TextField()
+    rate = models.IntegerField()
+    rate_type = models.IntegerField(choices=RATE_CHOICES)
+    skills = models.ManyToManyField(Skill)
+
+
+class Contractor(models.Model):
+    contact = models.OneToOneField(ContractorContactDetails)
+    skills = models.ManyToManyField(Skill)
+    cscs_card_number = models.IntegerField()
+    cscs_card_url = models.URLField()  # upload to S3
+
+    def skill_match(self):
+        return Role.objects.filter(role__skills__in=self.skills)
+
+    def location_match(self):
+        return Role.objects.filter(role__location__in=self.skills)
+
+
+class Location(models.Model):
+    description = models.TextField()
+    postcode = models.TextField(max_length=9)
+    role = models.ForeignKey(Role)
+
+
+class Hirer(models.Model):
+    company_name = models.CharField(max_length=30)
+    company_number = models.IntegerField(blank=True, null=True)
+
+
+class Agreement(models.Model):
+    date = models.DateField()
+    company = models.ForeignKey(Hirer)
+    pdf_agreement = models.URLField()  # upload to S3
+    rate = models.IntegerField()
+
+# Hirers have many roles
+# Candidates have many skills
+
+# Candidate search page (show me candidates in this location, with this skills set)
+# Role search (show me roles in this location, with this skills set)
